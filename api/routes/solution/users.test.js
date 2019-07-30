d82f1929b7fc88012f31b7fed569d14cd14ead4d
Jorge Lozano <jlozano9305@gmail.com>
2019-07-30 03:03:06 -0500

    adding spy sample

--- api/routes/solution/users.test.js
+++ api/routes/solution/users.test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 const sinon = require('sinon');
 const request = require('supertest');
 const app = require('../../app');
+const util = require('./../../util');
 
 
 describe('POST /userssample/subscribesample', () => {
@@ -10,7 +11,19 @@ describe('POST /userssample/subscribesample', () => {
   const validInput = { body: { email: 'asd@asd.com', name: 'Jorge' } };
   const invalidInput = { body: { foo: 'bar' } };
 
+  beforeEach(() => {
+    sinon.spy(util, "validateRequest");
+  });
+  afterEach(() => {
+    util.validateRequest.restore();
+  });
   describe('When the request has invalid input data', () => {
+    it('should call util.validateRequest to make the schema validation of the request', async () => {
+      const res = await request(app)
+        .post(enpointRoute)
+        .send(invalidInput.body);
+      expect(util.validateRequest.called).to.eql(true);
+    });
     it('should return a 400 status code', async () => {
       const res = await request(app)
         .post(enpointRoute)
