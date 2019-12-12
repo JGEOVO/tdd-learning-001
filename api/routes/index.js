130e87de222de805d12af1c971a9df846980679f
jorgelozano95 <jlozano9305@gmail.com>
2019-12-11 19:40:32 -0500

    adding env on the get

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', envvars: [process.env.JEST_STARE_COVERAGE_LINK, process.env.MONGO_URI ? true : false] });
 });
 
 module.exports = router;
