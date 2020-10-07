48ea905d906b8fd5d2103b6a0cf12f5d0c61f9b3
Jorge Lozano <jlozano9305@gmail.com>
2020-10-07 15:18:27 -0500

    Adding env log

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,6 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
-
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
